1f1637e2cc482aadc48b59cca23406cfd3f38a61
XuanThongN <thong.xuan.vft@gmail.com>
2023-09-29 03:57:17 +0700

    feat: import layout và tách các đoạn code  dùng chung

---
+++ src/main/java/com/xuanthongn/spring_quanlycongviec/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.xuanthongn.spring_quanlycongviec.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @RequestMapping("/")
+    public String Index() {
+        return "index";
+    }
+}
