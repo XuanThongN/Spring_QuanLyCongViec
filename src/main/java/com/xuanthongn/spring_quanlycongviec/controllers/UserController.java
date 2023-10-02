4fa6aee15d1a44d245222b1b166f5685fd362878
autarchical200 <autarchical200@gmail.com>
2023-10-02 19:44:18 +0700

    xong crud table user cơ bản

---
+++ src/main/java/com/xuanthongn/spring_quanlycongviec/controllers/UserController.java
@@ -0,0 +1,55 @@
+package com.xuanthongn.spring_quanlycongviec.controllers;
+
+import java.util.List;
+
+import com.xuanthongn.spring_quanlycongviec.entities.User;
+import com.xuanthongn.spring_quanlycongviec.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/list")
+    public String listUsers(Model theModel) {
+        List<User> theUsers = userService.findAll();
+        theModel.addAttribute("users", theUsers);
+        return "user/list-user";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+        User theUser = new User();
+        theModel.addAttribute("user", theUser);
+        return "user/add-user";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") User theUser) {
+        userService.save(theUser);
+        return "redirect:/user/list";
+    }
+
+    @GetMapping("/updateForm")
+    public String showFormForUpdate(@RequestParam("userId") int theId, Model theModel) {
+        User theUser = userService.findById(theId);
+        theModel.addAttribute("user", theUser);
+        return "user/edit-user";
+    }
+
+    @GetMapping("/delete")
+    public String deleteUser(@RequestParam("userId") int theId) {
+        userService.deleteById(theId);
+        return "redirect:/user/list";
+    }
+}
--- src/main/java/com/xuanthongn/spring_quanlycongviec/entities/User.java
+++ src/main/java/com/xuanthongn/spring_quanlycongviec/entities/User.java
@@ -1,133 +1,61 @@
-package com.xuanthongn.spring_quanlycongviec.entities;
+    package com.xuanthongn.spring_quanlycongviec.entities;
 
-import com.xuanthongn.spring_quanlycongviec.common.Constant;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+    import com.xuanthongn.spring_quanlycongviec.common.Constant;
+    import jakarta.persistence.*;
+    import jakarta.validation.constraints.Min;
+    import jakarta.validation.constraints.NotBlank;
+    import jakarta.validation.constraints.NotNull;
+    import jakarta.validation.constraints.Size;
+    import lombok.AllArgsConstructor;
+    import lombok.Builder;
+    import lombok.Data;
+    import lombok.EqualsAndHashCode;
+    import lombok.NoArgsConstructor;
+    import lombok.ToString;
+    import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.Collection;
-import java.util.Date;
-
-
-@Entity // dùng để khai báo với Spring Boot rằng đây là 1 entity biểu diễn table trong database
-@Data // annotation này sẽ tự động khai báo getter và setter cho class
-@AllArgsConstructor // dùng để khai báo constructor với tất cả các properties
-@NoArgsConstructor //
-@Builder
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
-
-    @NotNull
-    @NotBlank(message = "Tên người dùng không thể để trống")
-    private String name;
-
-    @NotNull(message = "Ngày sinh không thể để trống")
-    @Temporal(TemporalType.DATE)
-    private Date birthday;
+    import java.util.Collection;
+    import java.util.Date;
 
 
-    @NotNull
-    @NotBlank(message = "Tên người dùng không thể để trống")
-    private String username;
-    private String password;
-    private String roles;
+    @Entity // dùng để khai báo với Spring Boot rằng đây là 1 entity biểu diễn table trong database
+    @Data // annotation này sẽ tự động khai báo getter và setter cho class
+    @AllArgsConstructor // dùng để khai báo constructor với tất cả các properties
+    @NoArgsConstructor //
+    @Builder
+    public class User {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+            @Column(name = "id", nullable = false)
+            private Long id;
 
-    private String avatar;
+            @NotNull
+            @NotBlank(message = "Tên người dùng không thể để trống")
+            private String name;
+
+        @NotNull(message = "Ngày sinh không thể để trống")
+        @Temporal(TemporalType.DATE)
+        @DateTimeFormat(pattern = "yyyy-MM-dd") // Định dạng ngày tháng
+        private Date birthday;
 
 
 
-	// mappedBy trỏ tới tên biến tasks ở trong User.
-	@ManyToMany(mappedBy = "users")
-	// LAZY để tránh việc truy xuất dữ liệu không cần thiết. Lúc nào cần thì mới query
-	@EqualsAndHashCode.Exclude
-	@ToString.Exclude
-    private Collection<Task> tasks;
+            @NotNull
+            @NotBlank(message = "Tên người dùng không thể để trống")
+            private String username;
+            private String password;
+            private String roles;
 
-//	public User() {
-//		super();
-//		// TODO Auto-generated constructor stub
-//	}
+            private String avatar;
 
-//	public User(Long id, @NotNull @NotBlank(message = "Tên người dùng không thể để trống") String name,
-//			@NotNull @NotBlank(message = "Họ tên không thể để trống") @Size(min = 5, max = 1000) Date birthday,
-//			@NotNull @NotBlank(message = "Tên người dùng không thể để trống") String username, String password,
-//			String roles, Collection<Task> tasks) {
-//		super();
-//		this.id = id;
-//		this.name = name;
-//		this.birthday = birthday;
-//		this.username = username;
-//		this.password = password;
-//		this.roles = roles;
-//		this.tasks = tasks;
-//	}
 
-	public Long getId() {
-		return id;
-	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+            // mappedBy trỏ tới tên biến tasks ở trong User.
+            @ManyToMany(mappedBy = "users")
+            // LAZY để tránh việc truy xuất dữ liệu không cần thiết. Lúc nào cần thì mới query
+            @EqualsAndHashCode.Exclude
+            @ToString.Exclude
+            private Collection<Task> tasks;
 
-	public String getName() {
-		return name;
-	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Date getBirthday() {
-		return birthday;
-	}
-
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getRoles() {
-		return roles;
-	}
-
-	public void setRoles(String roles) {
-		this.roles = roles;
-	}
-
-	public Collection<Task> getTasks() {
-		return tasks;
-	}
-
-	public void setTasks(Collection<Task> tasks) {
-		this.tasks = tasks;
-	}
-
-    
-}
+    }
